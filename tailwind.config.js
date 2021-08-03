8476b99ee562039ab98a45dfc6ab93b1a254a71d
Erwin <erwin@remotedev.co>
2021-08-03 17:27:23 +0800

    Push uncommited changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -27,10 +27,25 @@ module.exports = {
         fontFamily: {
             title: ['Lora'],
             regular: ['Open Sans']
+        },
+        screens: {
+            sm: '640px',
+            md: '768px',
+            lg: '1024px',
+            xl: '1280px',
+        },
+        fontSize: {
+            xs: '0.75rem',
+            sm: '0.875rem',
+            base: '1rem',
+            lg: '1.125rem',
+            xl: '1.25rem',
+            '2xl': '1.5rem',
+            '3xl': '1.875rem',
+            '4xl': '2.25rem',
+            '5xl': '3rem',
+            '6xl': '4rem',
         }
     },
-    variants: {},
-    plugins: [
-        require('@tailwindcss/ui'),
-    ]
+    variants: {}
 };
